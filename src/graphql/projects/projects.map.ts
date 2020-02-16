eb0b3718cb4ab7fa3109de58abd0069c1839dc34
piotr54321 <ziolko.piotr@outlook.com>
2020-02-16 08:35:24 +0100

    Basic of the portfolio server (.env) correct 1

--- src/graphql/projects/projects.map.ts
+++ src/graphql/projects/projects.map.ts
@@ -2,5 +2,6 @@ import {resolver} from 'graphql-sequelize';
 import {projects} from "../../models";
 
 export const ProjectsMap = {
-    projects_technologies: resolver(projects.associations.projects_technologies)
+    projects_technologies: resolver(projects.associations.projects_technologies),
+    projects_images: resolver(projects.associations.projects_images)
 };
